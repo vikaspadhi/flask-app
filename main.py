c2f8e5a3dd5a85f39e3d5c36edd439bdc7137a28
vikaspadhi <vikaspadhi5@gmail.com>
2024-01-25 19:21:09 +0530

    added template and created home page

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("This is the main file")
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+# Configure the app to serve static files from the 'static' folder
+app.config['STATIC_FOLDER'] = 'static'
+
+@app.route('/')
+def index():
+    return render_template('index.html',ctx={'title':'JOBFLARE | HOME'})
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
